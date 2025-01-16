9070aee4b2d61a69e463b9c2739fe0a156108b05
davigamer987 <davimarchesiprimo@gmail.com>
2025-01-16 23:51:23 +0100

    sm8475-common: improve core assignment
    Values taken from RisingOS Pong tree

--- rootdir/bin/init.kernel.post_boot-cape.sh
+++ rootdir/bin/init.kernel.post_boot-cape.sh
@@ -55,8 +55,12 @@ echo 15 15 15 15 15 15 15 15 > /proc/sys/walt/sched_util_busy_hyst_cpu_util
 echo 325 > /proc/sys/walt/walt_low_latency_task_threshold
 
 # cpuset parameters
-echo 0-3 > /dev/cpuset/background/cpus
-echo 0-3 > /dev/cpuset/system-background/cpus
+echo 0-1 > /dev/cpuset/background/cpus
+echo 0-2 > /dev/cpuset/system-background/cpus
+echo 0-6 > /dev/cpuset/foreground/cpus
+
+# Set restricted cpuset to the same CPUs as system-background
+cat /dev/cpuset/system-background/cpus > /dev/cpuset/restricted/cpus
 
 # Turn off scheduler boost at the end
 echo 0 > /proc/sys/walt/sched_boost
